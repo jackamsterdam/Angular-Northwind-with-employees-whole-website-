a1ef837ea85bd1e89c33481cd1fcf121f8f7ab7e
Jack Amsterdam <64046793+jackamsterdam@users.noreply.github.com>
2022-04-03 08:04:41 +0300

    Basic Layout Shoe Store in Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout-area/layout/layout.component';
+import { HeaderComponent } from './components/layout-area/header/header.component';
+import { FooterComponent } from './components/layout-area/footer/footer.component';
+import { MenuComponent } from './components/layout-area/menu/menu.component';
+import { HomeComponent } from './components/home-area/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LayoutComponent]
 })
 export class AppModule { }
