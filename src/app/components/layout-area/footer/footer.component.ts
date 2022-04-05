23fa609be07e0f31edd51261f14c0cf1d32986a6
Jack Amsterdam <64046793+jackamsterdam@users.noreply.github.com>
2022-04-05 22:34:54 +0300

    footer date but in english using pipe instead

--- src/app/components/layout-area/footer/footer.component.ts
+++ src/app/components/layout-area/footer/footer.component.ts
@@ -6,20 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+    monthandyear: Date = new Date()
   constructor() { }
 
   ngOnInit(): void {
   }
-  month: string[] = ["ינואר","פברואר","מרץ","אפריל","מאי","יוני","יולי","אוגוסט","ספטמבר","אוקטובר","נובמבר","דצמבר"];
+
+  // you dont need all this maybe only in react cause in angular we have pipes 
+  // month: string[] = ["ינואר","פברואר","מרץ","אפריל","מאי","יוני","יולי","אוגוסט","ספטמבר","אוקטובר","נובמבר","דצמבר"];
   
   
-  getFullYearAndMonth(): string {
-    const d = new Date()
-    const year = d.getFullYear()
-    const numMonth = d.getMonth()
-    const monthName = this.month[numMonth]
-    return monthName + ' ' + year
-  }
+  // getFullYearAndMonth(): string {
+  //   const d = new Date()
+  //   const year = d.getFullYear()
+  //   const numMonth = d.getMonth()
+  //   const monthName = this.month[numMonth]
+  //   return monthName + ' ' + year
+  // }
 
 }
